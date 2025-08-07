0e0d1347e54c4e67151734668762679d6376c67a
BarinderSingh30 <78806961+BarinderSingh30@users.noreply.github.com>
2025-08-07 19:34:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
+++ Difficulty: Medium/Longest Subarray with Sum K/longest-subarray-with-sum-k.cpp
@@ -2,33 +2,31 @@ class Solution {
   public:
     int longestSubarray(vector<int>& arr, long long k) {
         // code here
-        unordered_map<long long,int> presum;
-        
+        unordered_map<int,int> presum;
         long long sum = 0;
         int maxlen = 0;
         
         
+        
         for(int i =0;i<arr.size();i++){
+            sum+=arr[i];
             
-            // sum for the prefix sum
-            sum +=arr[i];
-            
-            //if we get sum k from the subarray of first element.
-            if(sum == k){
-                maxlen = max(maxlen, i+1);
+            if(sum ==k){
+                maxlen = max(maxlen,i+1);
             }
             
-            //reverse engineering trick check in hashmap if we find remaining (sum -k) in the map
             long long rem = sum - k;
             
             if(presum.find(rem)!=presum.end()){
-                int len = i - presum[rem];
-                maxlen = max(maxlen,len);
+                maxlen = max(maxlen,i-presum[rem]);
             }
             
             if(presum.find(sum)==presum.end()){
-                presum[sum] = i;
+            presum[sum] = i;
             }
+            
+            
+            
             
         }
         return maxlen;
