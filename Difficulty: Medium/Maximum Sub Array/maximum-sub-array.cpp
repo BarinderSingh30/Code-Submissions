9360b97e8927570a54514bfaeb759152b1225f3f
Barinder Singh <78806961+BarinderSingh30@users.noreply.github.com>
2025-09-02 18:42:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Maximum Sub Array/maximum-sub-array.cpp
+++ Difficulty: Medium/Maximum Sub Array/maximum-sub-array.cpp
@@ -1,34 +1,49 @@
 // User function template for C++
 class Solution {
   public:
-    vector<int> findSubarray(vector<int>& a) {
+    vector<int> findSubarray(vector<int>& arr) {
         // code here
-    int n =a.size();
-    vector<int>ans, temp;
-    long long max_sum=0, curr_sum=0;
-    
-    for(int i=0; i<n; i++){
-        if(a[i]<0){
-            temp.clear();
-            curr_sum=0;
-        }
-        else{
-            temp.push_back(a[i]);
-            curr_sum+=a[i];
+        vector<int> ans;
+        
+        long long sum = 0;
+        long long maxsum = LONG_MIN;
+        int start = -1;
+        
+        int ansstart = -1, ansend = -1;
+        
+        for(int i =0;i<arr.size();i++){
+            if(sum == 0){
+                start =i;
+            }
             
-            if(curr_sum>max_sum){
-                max_sum=curr_sum;
-                ans=temp;
+            sum+=arr[i];
+            
+            if(sum>maxsum && arr[i]>=0){
+                maxsum = sum;
+                ansstart = start;
+                ansend = i;
             }
-            else if(curr_sum==max_sum){
-                if(temp.size()>ans.size()){
-                   ans=temp;
-                }
+            
+            if(sum==maxsum && arr[i]==0){
+                maxsum = sum;
+                ansstart = start;
+                ansend = i;
+            }
+
+            
+            
+            if(arr[i]<0){
+                sum = 0;
             }
         }
-    }
-    
-    if(ans.empty()) ans.push_back(-1);
-    return ans;
+        
+        if(ansstart == -1){
+            return {-1};
+        }
+        for(int i = ansstart;i<=ansend;i++){
+            ans.push_back(arr[i]);
+        }
+        
+        return ans;
     }
 };
