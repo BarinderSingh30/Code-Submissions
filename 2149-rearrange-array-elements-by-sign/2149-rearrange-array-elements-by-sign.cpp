7be016ddefdc7cc80ade54d370d708047e7a0d31
Barinder Singh <78806961+BarinderSingh30@users.noreply.github.com>
2025-09-05 18:52:52 +0530

    Medium

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -4,18 +4,22 @@ public:
         int n = nums.size();
         vector<int> ans(n,0);
 
-        int pindex = 0, nindex =1;
+        int pindex = 0;
+        int nindex = 1;
 
         for(int i =0;i<n;i++){
-            if(nums[i]<0){
-                ans[nindex] = nums[i];
-                nindex+=2;
-            }
-            else{
+            if(nums[i]>0){
                 ans[pindex] = nums[i];
                 pindex+=2;
             }
+            else{
+                ans[nindex] = nums[i];
+                nindex+=2;
+            }
         }
+        
+
+
         return ans;
     }
 };
